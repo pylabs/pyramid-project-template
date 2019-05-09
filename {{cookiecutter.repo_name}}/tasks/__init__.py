5557614538503d6e4b6d2f69f5ca76de2d480252
William Wu <william@pylabs.org>
2019-05-09 18:48:19 +0800

    Let deploy task can omit to specify ini file path

--- {{cookiecutter.repo_name}}/tasks/__init__.py
+++ {{cookiecutter.repo_name}}/tasks/__init__.py
@@ -1,15 +1,24 @@
-import re
+import os, re
 
 from invoke import Collection, task
+from invoke.exceptions import ParseError
 
 from {{ cookiecutter.repo_name }}.tests.helper import get_ini_settings, import_test_db_data
 
 from . import db, test
 
 
-@task
-def deploy(c, ini_file):
+@task(optional=['ini_file'])
+def deploy(c, ini_file=None):
     """Deploy project"""
+
+    if ini_file is None:
+        if os.path.exists('production.ini'):
+            ini_file = 'production.ini'
+        elif os.path.exists('development.ini'):
+            ini_file = 'development.ini'
+        else:
+            raise ParseError('--ini-file should be valid ini file path')
 
     c.run('git pull origin master')
     c.run('systemctl stop uwsgi')
