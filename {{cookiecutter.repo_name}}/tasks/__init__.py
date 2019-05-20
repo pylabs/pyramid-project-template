d56d92c78bfb85cd6e95c643092383e1808b17c4
William Wu <william@pylabs.org>
2019-05-20 16:52:03 +0800

    Fix wrong pyinvoke task usage

--- {{cookiecutter.repo_name}}/tasks/__init__.py
+++ {{cookiecutter.repo_name}}/tasks/__init__.py
@@ -8,17 +8,9 @@ from {{ cookiecutter.repo_name }}.tests.helper import get_ini_settings, import_t
 from . import db, test
 
 
-@task(optional=['ini_file'])
-def deploy(c, ini_file=None):
+@task
+def deploy(c, ini_file):
     """Deploy project"""
-
-    if ini_file is None:
-        if os.path.exists('production.ini'):
-            ini_file = 'production.ini'
-        elif os.path.exists('development.ini'):
-            ini_file = 'development.ini'
-        else:
-            raise ParseError('--ini-file should be valid ini file path')
 
     c.run('git pull origin master')
     c.run('systemctl stop uwsgi')
