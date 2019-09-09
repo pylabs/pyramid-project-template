e94ccf8f6aa73c35159c3cb2996a6c3dfea034bb
William Wu <william@pylabs.org>
2019-09-09 11:22:16 +0800

    deploy task can detect ini path

--- {{cookiecutter.repo_name}}/tasks/__init__.py
+++ {{cookiecutter.repo_name}}/tasks/__init__.py
@@ -1,4 +1,4 @@
-import os, re
+import re
 
 from invoke import Collection, task
 from invoke.exceptions import ParseError
@@ -6,11 +6,15 @@ from invoke.exceptions import ParseError
 from {{ cookiecutter.repo_name }}.tests.helper import get_ini_settings, import_test_db_data
 
 from . import db, test
+from .helper import find_ini_file
 
 
-@task
-def deploy(c, ini_file):
+@task(optional=['ini_file'])
+def deploy(c, ini_file=None):
     """Deploy project"""
+
+    if ini_file is None:
+        ini_file = find_ini_file()
 
     c.run('git pull origin master')
     c.run('systemctl stop uwsgi')
